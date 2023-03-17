59c63ff62d4cdfaecd5657a766482bdc252a2408
Kev1nX <kevin0706x@gmail.com>
2023-03-17 23:11:43 +1100

    Uploading

---
+++ src/components/Contract.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="contract">
+        <!-- <h3>{{ contract.name }}</h3>
+        <h4>Email: {{ contract.email}}</h4>
+        <h4>Phone number: {{ contract.phone }}</h4>
+        <h4>Website: {{ contract.website }}</h4>
+        <h4>{{ contract.company }}</h4> -->
+        <v-expansion-panels variant="popout" class="my-4">
+          <v-expansion-panel>
+            <v-expansion-panel-title class = "panel-title">{{ contract.name }}</v-expansion-panel-title>
+                <v-expansion-panel-text>Email : {{ contract.email}}</v-expansion-panel-text>
+                <v-expansion-panel-text>Phone number : {{ contract.phone }}</v-expansion-panel-text>
+                <v-expansion-panel-text>Website : {{ contract.website }}</v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+    </div>
+</template>
+
+<script>
+export default{
+    name :'Contract',
+    props:{
+        contract: Object,
+    },
+
+}
+</script>
+<style scoped>
+.panel-title{
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: larger;
+}
+.contract {
+  background: #d3d2d2;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.contract h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+h4{
+  color:#000000;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <h1>
+            Contracts
+        </h1>
+    </header>
+</template>
+
+<script>
+export default{
+    name:"Header",
+}
+</script>
+<style>
+h1{
+    text-align: center;
+}
+</style>
